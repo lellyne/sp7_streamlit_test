eb5205eb5c63852e28507329c41a79cda253486a
lellyne <lissandra.ellyne@gmail.com>
2026-01-12 11:39:32 +0100

    added random coin toss to app.py

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
+# agregar la var chart para el grafico de lineas y la funcion tc para emular el lanzamiento de una moneda n veces y calcula la media en cada nueva iteración, que se añade a chart (como una nueva observación)
+
+import scipy.stats
  
 import streamlit as st 
- 
+
+import time
+
 st.header('Lanzar una moneda') 
+
+chart = st.line_chart([0.5])
+
+def toss_coin(n): # funcion para emular el lanzamiento de una moneda
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0 
+    outcome_1_count = 0
+
+    for r in trial_outcomes: 
+        outcome_no +=1
+        if r == 1:
+            outcome_1_count +=1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+    
+    return mean
+
 
 number_of_trials  = st.slider('Numero de intentos?', 1, 1000, 10)
 start_button = st.button('Ejecutar')
+
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
 
-st.write('Esta aplicacion aun no es funcional. En construccion.') 
+# st.write('Esta aplicacion aun no es funcional. En construccion.') 
